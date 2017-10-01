be7bc24442e266fc3c4eb7245a6a22748a3b5cb9
Anderson Bittencourt <anderson_btt@hotmail.com>
2017-09-30 21:39:52 -0300

    Created POST and GET method for a single Categoria resource

--- src/main/java/br/com/bitclouds/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/br/com/bitclouds/algamoney/api/resource/CategoriaResource.java
@@ -1,11 +1,21 @@
 package br.com.bitclouds.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.bitclouds.algamoney.api.model.Categoria;
 import br.com.bitclouds.algamoney.api.repository.CategoriaRepository;
@@ -18,8 +28,33 @@ public class CategoriaResource {
 	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
-	public List<Categoria> buscarCategorias(){
-		return(categoriaRepository.findAll());
+	public ResponseEntity<?> buscarCategorias(){
+		List<Categoria> categorias = categoriaRepository.findAll();
+		return ResponseEntity.ok(categorias);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria entity, HttpServletResponse response){
+		Categoria categoria = categoriaRepository.save(entity);
+		
+		URI uri = ServletUriComponentsBuilder
+						.fromCurrentRequestUri()
+						.path("/{codigo}")
+						.buildAndExpand(categoria.getCodigo())
+						.toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoria);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo){
+		Categoria categoria = categoriaRepository.findOne(codigo);
+		if(null==categoria){
+			return(ResponseEntity.notFound().build());
+		}else{
+			return(ResponseEntity.ok(categoria));
+		}
 	}
 
 }
